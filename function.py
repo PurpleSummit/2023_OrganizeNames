09d43b951c23b0f53f95abc24880db5c19f6c502
Promise <109252183+PurpleSummit@users.noreply.github.com>
2025-10-15 14:14:06 -0400

    Refactor variable name from OC to person

--- function.py
+++ function.py
@@ -3,8 +3,8 @@ names = []
 with open("names.csv") as file:
     for line in file:
         name, category = line.rstrip().split(",")
-        OC = {"name": name, "category": category}
-        names.append(OC)
+        person = {"name": name, "category": category}
+        names.append(person)
 
 for n in sorted(names, key=lambda p: (p["category"], p["name"])):
     print(f"{n['name']} - {n['category']}")
